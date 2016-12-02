9e581d94c7f1691b07023167df76d4a46fdf6a10
pgrho <shipwreck@live.jp>
2016-12-03 00:32:45 +0900

    Initial Commit

---
+++ src/DockingManager/script/DockingManager.ts
@@ -0,0 +1,27 @@
+﻿module dock {
+
+    interface DockingManagerSettings {
+        backgroungClassName?: string;
+    }
+
+    export class DockingManager {
+
+        private _root: Element;
+
+        public backgroungClassName: string;
+
+        constructor(element: string, settings?: DockingManagerSettings)
+        constructor(element: Element, settings?: DockingManagerSettings)
+        constructor(element: any, settings?: DockingManagerSettings) {
+            this._root = (<Element>element).innerHTML !== undefined ? element : document.querySelector(element);
+
+            var s = settings || {};
+
+            this.backgroungClassName = s.backgroungClassName || "docking-manager-background";
+        }
+
+        public init() {
+            this._root.classList.add(this.backgroungClassName);
+        }
+    }
+}
